031e878e1820b07031bdb5ad083f99d3fe6916f0
linzhlalala <zhihonglin90@gmail.com>
2019-10-15 18:40:32 +1100

    first edition

---
+++ precache-manifest.43b045aff78b8164b520c03050a6724b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c94a10a55c52dd863c42930861ecfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9420e50d47c0ee8c4b3b",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d03bc30fb4f66837ecae",
+    "url": "/static/js/2.a49a7a63.chunk.js"
+  },
+  {
+    "revision": "9420e50d47c0ee8c4b3b",
+    "url": "/static/js/main.5613ec75.chunk.js"
+  },
+  {
+    "revision": "4e7fa004b434045c4927",
+    "url": "/static/js/runtime-main.4ff6f946.js"
+  }
+]);
